7ea33cfece1d7f80b02a4ba16a8f5f575e771de9
Chiaki_meow <splendid_hero@foxmail.com>
2023-05-31 21:02:27 +0800

    half way to zbuffer

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@
 #include "model.h"
 #include "geometry.h"
 
-
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 const TGAColor green   = TGAColor(0, 255,   0,   255);
+const TGAColor blue   = TGAColor(0, 0,   255,   255);
 Model *model = NULL;
 const int width = 800;
 const int height = 800;
@@ -125,6 +125,32 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color){
 //    line(t2, t0, image, red);
 }
 
+void rasterize(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color, int ybuffer[]){
+    if(p0.x>p1.x){
+        std::swap(p0, p1);
+    }
+    for(int x=p0.x; x<p1.x; x++){
+        float t = (x - p0.x)/(float)(p1.x-p0.x);
+        int y = p0.y*(1.-t)+p1.y*t;
+        if(ybuffer[x]<y){
+            ybuffer[x] = y;
+            image.set(x, 0, color);
+        }
+    }
+}
+
+void triangle(Vec3f *pts, float *zbuffer, TGAImage &image, TGAColor color){
+    Vec2f bboxmin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+    Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
+    Vec2f clamp(image.get_width()-1, image.get_height()-1);
+    for(int i=0;i<3;i++){
+        for(int j=0;j<2;j++){
+            bboxmin[j] = std::max(0.f,      std::min(bboxmin[j], pts[i][j]));
+            bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
+        }
+    }
+}
+
 int main(int argc, char** argv) {
     if(2==argc)
         model = new Model(argv[1]);
@@ -133,54 +159,118 @@ int main(int argc, char** argv) {
 
 	TGAImage image(width, height, TGAImage::RGB);
 
+// better way to show triangle
+/*
     TGAImage frame(200, 200, TGAImage::RGB);
     Vec2i pts[3] = {Vec2i(10,10), Vec2i(100, 30), Vec2i(190, 160)};
     triangle(pts, frame, TGAColor(255, 0, 0, 255));
     frame.flip_vertically(); // to place the origin i
 
-//    line(13, 20, 80, 40, image, white);
-//    line(20, 13, 40, 80, image, red);
-//    line(80, 40, 13, 20, image, red);
+    frame.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    frame.write_tga_file("framebuffer.tga");
+*/
 
-//    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
-//    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
-//    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
-//    triangle(t0[0], t0[1], t0[2], image, red);
-//    triangle(t1[0], t1[1], t1[2], image, white);
-//    triangle(t2[0], t2[1], t2[2], image, green);
+/*
+    line(13, 20, 80, 40, image, white);
+    line(20, 13, 40, 80, image, red);
+    line(80, 40, 13, 20, image, red);
+
+    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
+    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
+    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+    triangle(t0[0], t0[1], t0[2], image, red);
+    triangle(t1[0], t1[1], t1[2], image, white);
+    triangle(t2[0], t2[1], t2[2], image, green);
+     */
 
 // line model renderer
-
-//    for(int i=0; i<model->nfaces(); i++){
-//        std::vector<int> face = model->face(i);
-//        for(int j=0;j<3;j++){
-//            Vec3f v0 = model->vert(face[j]);
-//            Vec3f v1 = model->vert(face[(j+1)%3]);
-//            int x0 = (v0.x+1.)*width/2.;
-//            int y0 = (v0.y+1.)*width/2.;
-//            int x1 = (v1.x+1.)*width/2.;
-//            int y1 = (v1.y+1.)*width/2.;
-//            line(Vec2i(x0,y0),Vec2i(x1,y1),image,white);
-//        }
-//    }
+/*
+    for(int i=0; i<model->nfaces(); i++){
+        std::vector<int> face = model->face(i);
+        for(int j=0;j<3;j++){
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j+1)%3]);
+            int x0 = (v0.x+1.)*width/2.;
+            int y0 = (v0.y+1.)*width/2.;
+            int x1 = (v1.x+1.)*width/2.;
+            int y1 = (v1.y+1.)*width/2.;
+            line(Vec2i(x0,y0),Vec2i(x1,y1),image,white);
+        }
+    }
+    */
 
 // colorful model renderer
+/*
+    for(int i=0; i<model->nfaces(); i++){
+        std::vector<int> face = model->face(i);
+        Vec2i screen_coords[3];
+        for(int j=0; j<3; j++){
+            Vec3f world_coords = model->vert(face[j]);
+            screen_coords[j] = Vec2i ((world_coords.x+1.)*width/2., (world_coords.y+1.)*height/2.);
+        }
+        triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(rand()%255,rand()%255,rand()%255,255));
+    }
+*/
 
-//    for(int i=0; i<model->nfaces(); i++){
-//        std::vector<int> face = model->face(i);
-//        Vec2i screen_coords[3];
-//        for(int j=0; j<3; j++){
-//            Vec3f world_coords = model->vert(face[j]);
-//            screen_coords[j] = Vec2i ((world_coords.x+1.)*width/2., (world_coords.y+1.)*height/2.);
-//        }
-//        triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(rand()%255,rand()%255,rand()%255,255));
-//    }
+// simple light renderer : the intensity of illumination is equal to the scalar product of the light vector and the normal to the given triangle
+// 光照强度：面的法向量与光照方向的点乘
+/*
+    Vec3f light_dir(0,0,-1);
+
+    for(int i=0;i<model->nfaces();i++){
+        std::vector<int> face = model->face(i);
+        Vec2i screen_coords[3];
+        Vec3f world_coords[3];
+        for(int j=0;j<3;j++){
+            Vec3f v = model->vert(face[j]);
+            screen_coords[j] = Vec2i ((v.x+1.)*width/2., (v.y+1.)*height/2.);
+            world_coords[j] = v;
+        }
+        Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
+        n.normalize();
+        float intensity = n*light_dir;
+        if(intensity>0){
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
+        }
+    }
+    */
+
+// y-buffer
+/*
+    {
+        TGAImage scene(width, height, TGAImage::RGB);
+        // scene 2d mesh
+        line(Vec2i(20, 34),   Vec2i(744, 400), scene, red);
+        line(Vec2i(120, 434), Vec2i(444, 400), scene, green);
+        line(Vec2i(330, 463), Vec2i(594, 200), scene, blue);
+
+        // screen line
+        line(Vec2i(10, 10), Vec2i(790, 10), scene, white);
+
+        scene.flip_vertically();
+        scene.write_tga_file("scene.tga");
+    }
+
+    TGAImage render(width, 16, TGAImage::RGB);
+    int ybuffer[width];
+    for(int i=0; i<width; i++){
+        ybuffer[i] = std::numeric_limits<int>::min();
+    }
+
+    rasterize(Vec2i(20, 34),   Vec2i(744, 400), render, red,   ybuffer);
+    rasterize(Vec2i(120, 434), Vec2i(444, 400), render, green, ybuffer);
+    rasterize(Vec2i(330, 463), Vec2i(594, 200), render, blue,  ybuffer);
+
+
+    render.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    render.write_tga_file("output.tga");
+*/
+
+    triangle(screen_coords, float *zbuffer, image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
 
-    frame.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-    frame.write_tga_file("framebuffer.tga");
 	delete model;
 	return 0;
 }
