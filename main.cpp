86eb0dda0202950a0107170eb84cfdbea7d6d7c5
Chiaki_meow <splendid_hero@foxmail.com>
2023-05-31 18:01:19 +0800

    Update colorful model

--- main.cpp
+++ main.cpp
@@ -59,6 +59,36 @@ void line(Vec2i t0, Vec2i t1, TGAImage &image, TGAColor color){
     }
 }
 
+Vec3f barycentric(Vec2i* pts, Vec2i P) {
+    Vec3f u = Vec3f(pts[2].x-pts[0].x, pts[1].x-pts[0].x, pts[0].x-P.x)^Vec3f(pts[2].y-pts[0].y, pts[1].y-pts[0].y, pts[0].y-P.y);
+    /* `pts` and `P` has integer value as coordinates
+       so `abs(u[2])` < 1 means `u[2]` is 0, that means
+       triangle is degenerate, in this case return something with negative coordinates */
+    if (std::abs(u.z)<1) return Vec3f(-1,1,1);
+    return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
+}
+
+void triangle(Vec2i *pts, TGAImage &image, TGAColor color) {
+    Vec2i bboxmin(image.get_width()-1,  image.get_height()-1);
+    Vec2i bboxmax(0, 0);
+    Vec2i clamp(image.get_width()-1, image.get_height()-1);
+    for (int i=0; i<3; i++) {
+        bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
+        bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
+
+        bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
+        bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
+    }
+    Vec2i P;
+    for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) {
+        for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) {
+            Vec3f bc_screen  = barycentric(pts, P);
+            if (bc_screen.x<0 || bc_screen.y<0 || bc_screen.z<0) continue;
+            image.set(P.x, P.y, color);
+        }
+    }
+}
+
 void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color){
     // 对顶点排序，按照y的大小从高到低排序 => y0<y1<y2
     if(t0.y>t1.y)   std::swap(t0,t1);
@@ -103,16 +133,23 @@ int main(int argc, char** argv) {
 
 	TGAImage image(width, height, TGAImage::RGB);
 
+    TGAImage frame(200, 200, TGAImage::RGB);
+    Vec2i pts[3] = {Vec2i(10,10), Vec2i(100, 30), Vec2i(190, 160)};
+    triangle(pts, frame, TGAColor(255, 0, 0, 255));
+    frame.flip_vertically(); // to place the origin i
+
 //    line(13, 20, 80, 40, image, white);
 //    line(20, 13, 40, 80, image, red);
 //    line(80, 40, 13, 20, image, red);
 
-    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
-    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
-    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
-    triangle(t0[0], t0[1], t0[2], image, red);
-    triangle(t1[0], t1[1], t1[2], image, white);
-    triangle(t2[0], t2[1], t2[2], image, green);
+//    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
+//    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
+//    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+//    triangle(t0[0], t0[1], t0[2], image, red);
+//    triangle(t1[0], t1[1], t1[2], image, white);
+//    triangle(t2[0], t2[1], t2[2], image, green);
+
+// line model renderer
 
 //    for(int i=0; i<model->nfaces(); i++){
 //        std::vector<int> face = model->face(i);
@@ -123,12 +160,27 @@ int main(int argc, char** argv) {
 //            int y0 = (v0.y+1.)*width/2.;
 //            int x1 = (v1.x+1.)*width/2.;
 //            int y1 = (v1.y+1.)*width/2.;
-//            line(x0,y0,x1,y1,image,white);
+//            line(Vec2i(x0,y0),Vec2i(x1,y1),image,white);
 //        }
 //    }
 
-	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-	image.write_tga_file("output.tga");
+// colorful model renderer
+
+//    for(int i=0; i<model->nfaces(); i++){
+//        std::vector<int> face = model->face(i);
+//        Vec2i screen_coords[3];
+//        for(int j=0; j<3; j++){
+//            Vec3f world_coords = model->vert(face[j]);
+//            screen_coords[j] = Vec2i ((world_coords.x+1.)*width/2., (world_coords.y+1.)*height/2.);
+//        }
+//        triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(rand()%255,rand()%255,rand()%255,255));
+//    }
+
+    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    image.write_tga_file("output.tga");
+
+    frame.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    frame.write_tga_file("framebuffer.tga");
 	delete model;
 	return 0;
 }
