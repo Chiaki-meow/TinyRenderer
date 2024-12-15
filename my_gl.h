04cabbca992456607e6622bdf9ba3c356befea99
Chiaki-meow <chiaki_meow@sjtu.edu.cn>
2024-12-15 16:11:45 +0800

    upd: add self_gl method and textures

---
+++ my_gl.h
@@ -0,0 +1,27 @@
+//
+// Created by Chiaki_meow on 2024/12/12.
+//
+
+#ifndef TINYRENDERER_MY_GL_H
+#define TINYRENDERER_MY_GL_H
+
+#include "tgaimage.h"
+#include "geometry.h"
+
+extern Matrix ModelView;
+extern Matrix Viewport;
+extern Matrix Projection;
+
+void viewport(int x, int y, int w, int h);
+void projection(float coeff = 0.f);
+void lookat(Vec3f eye, Vec3f center, Vec3f up);
+
+struct IShader {
+    virtual ~IShader() {}
+    virtual Vec4f vertex(int iface, int nthvert) = 0;
+    virtual bool fragment(Vec3f bar, TGAColor &color) = 0;
+};
+
+void triangle(Vec4f *pts, IShader &shader, TGAImage &image, TGAImage &zbuffer);
+
+#endif //TINYRENDERER_MY_GL_H
